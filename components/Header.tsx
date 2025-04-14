3a5c49e6ae1ed1e845bb8f1dbc8c7ae49840aecb
Mohamad Hosein <m.hosein.dev@example.com>
2025-04-14 12:16:31 +0330

    Update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,23 @@
 "use client";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
-import { FaSearch, FaShoppingBasket } from "react-icons/fa";
+import { FaArrowLeft, FaSearch, FaShoppingBasket } from "react-icons/fa";
 import { FaTruckFast } from "react-icons/fa6";
 
 export default function Header() {
   const [showBox, setShowBox] = useState(false);
   const [showBox2, setShowBox2] = useState(false);
   const [showBox3, setShowBox3] = useState(false);
-
+  const curentUrl = usePathname();
   return (
     <header>
       <section className=" bg-[url('https://t-theme.com/foodking/wp-content/uploads/2024/02/hero-bg-3.jpg')]  bg-cover bg-center ">
-        <div className="border-b-[1px] border-[#f4f1ea42] ">
-          <div className="  text-[#fcfbfe] mx-auto max-w-[82rem] py-[2rem] items-center flex justify-between px-[1.5rem] ">
+        <div className={` ${curentUrl == "/" ? "border-b-[1px] border-[#f4f1ea42] " : " border-none "} `}>
+          <div className={`  text-[#fcfbfe] mx-auto max-w-[82rem] ${curentUrl === "/" ? "py-[2rem]" : " py-[1.5rem] "}  items-center flex justify-between px-[1.5rem] `}>
             <div className=" flex items-center gap-[5rem] ">
-              <button className=" outline-none transition ease-in-out delay-200 cursor-pointer hover:bg-[#00813D] bg-[#D12525] flex items-center justify-center gap-[.5rem] border-[1px] border-[#ffff] text-center px-[1rem] w-[15rem] py-[1rem] pb-[1.3rem] font-bold ">
+              <button className=" outline-none  transition-all ease delay-200 cursor-pointer hover:bg-[#00813D] bg-[#D12525] flex items-center justify-center gap-[.5rem] border-[1px] border-[#ffff] text-center px-[1rem] w-[15rem] py-[1rem] pb-[1.3rem] font-bold ">
                 اکنون سفارش دهید
                 <span>
                   <FaTruckFast size={22} />{" "}
@@ -43,7 +44,7 @@ export default function Header() {
                   className="  cursor-pointer     gap-[.2rem]  font-semibold flex  items-center flex-row-reverse "
                 >
                   <span className=" hover:text-[#00813D] transition ease-in delay-150 font-semibold"> صفحات</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className={` ${showBox ? " rotate-180 " : " rotate-none  cursor-pointer    "} transition-transform duration-500 hover:rotate-180  mt-[.4rem]  size-[12.5px]  `}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className={` ${showBox ? " rotate-180 " : " rotate-none  cursor-pointer    "} transition-transform duration-500   mt-[.4rem]  size-[12.5px]  `}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                   </svg>{" "}
                 </div>
@@ -51,7 +52,7 @@ export default function Header() {
                   <Link onClick={() => setShowBox(false)} className="  hidden text-[#515151] hover:text-[#ffff] transition ease-in delay-150 hover:bg-[#D12525] py-[1rem]  border-b-[1px] border-b-[#eaeaea]" href={"/shop/chef"}>
                     <span className="  font-semibold    px-[1rem]  "> اشپز </span>
                   </Link>
-                  <Link onClick={() => setShowBox(false)} className="  text-[#515151] hover:text-[#ffff] transition ease-in delay-150 hover:bg-[#D12525] py-[1rem]  border-b-[1px] border-b-[#eaeaea]    " href={"/food-menu"}>
+                  <Link onClick={() => setShowBox(false)} className="  text-[#515151] hover:text-[#ffff] transition ease-in delay-150 hover:bg-[#D12525] py-[1rem]  border-b-[1px] border-b-[#eaeaea]    " href={"/shop/chef"}>
                     <span className="  font-semibold    px-[1rem]  "> اشپز </span>
                   </Link>
                   <Link onClick={() => setShowBox(false)} className="  text-[#515151] hover:text-[#ffff] transition ease-in delay-150 hover:bg-[#D12525] py-[1rem]  border-b-[1px] border-b-[#eaeaea]    " href={""}>
@@ -82,7 +83,7 @@ export default function Header() {
                   className="  cursor-pointer     gap-[.2rem]  font-semibold flex  items-center flex-row-reverse "
                 >
                   <span className=" hover:text-[#00813D] transition ease-in delay-150 font-semibold"> وبلاگ</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className={` ${showBox2 ? " rotate-180 " : " rotate-none  cursor-pointer    "} transition-transform duration-500 hover:rotate-180  mt-[.4rem]  size-[12.5px]  `}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className={` ${showBox2 ? " rotate-180 " : " rotate-none  cursor-pointer    "} transition-transform duration-500   mt-[.4rem]  size-[12.5px]  `}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                   </svg>{" "}
                 </div>
@@ -102,8 +103,8 @@ export default function Header() {
                   }}
                   className="  cursor-pointer     gap-[.2rem]  font-semibold flex  items-center flex-row-reverse "
                 >
-                  <span className=" hover:text-[#00813D] transition ease-in delay-150 font-semibold"> فروشگاه</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className={` ${showBox3 ? " rotate-180 " : " rotate-none  cursor-pointer    "} transition-transform duration-500 hover:rotate-180  mt-[.4rem]  size-[12.5px]  `}>
+                  <span className="  transition ease-in delay-150 font-semibold"> فروشگاه</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className={` ${showBox3 ? " rotate-180 " : " rotate-none  cursor-pointer    "} transition-transform duration-500   mt-[.4rem]  size-[12.5px]  `}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                   </svg>{" "}
                 </div>
@@ -132,7 +133,7 @@ export default function Header() {
               <Link className="hover:text-[#00813D]  cursor-pointer    transition ease-in delay-150 font-semibold" href={"/about-us"}>
                 درباره ما{" "}
               </Link>
-              <Link className="   cursor-pointer   gap-[.2rem] font-semibold  flex  items-center flex-row-reverse " href={""}>
+              <Link className="   cursor-pointer   gap-[.2rem] font-semibold  flex  items-center flex-row-reverse " href={"/"}>
                 <span className=" hover:text-[#00813D] transition ease-in delay-150 font-semibold"> صفحه اصلی</span>
               </Link>
             </div>
@@ -140,25 +141,39 @@ export default function Header() {
           </div>
         </div>
 
-        <div className=" pt-[4rem] mx-auto max-w-[82rem] text-end  py-[.8rem] items-center flex justify-between px-[1.5rem]  ">
-          <div>
-            <motion.img
-              src="https://modinatheme.com/foodking/wp-content/uploads/2024/02/pizza-4.png" 
-              alt="Pizza"
-              className=" w-[80%] " 
-              animate={{ rotate: 360 }}
-              transition={{
-                repeat: Infinity,
-                duration: 20, 
-                ease: "linear"
-              }}
-            />
+        {curentUrl === "/" && (
+          <div className=" relative pt-[4rem] mx-auto max-w-[82rem] text-end  py-[3rem] items-center flex justify-between px-[1.5rem]  ">
+            <div>
+              <motion.img
+                src="https://modinatheme.com/foodking/wp-content/uploads/2024/02/pizza-4.png" // یا هر آدرس تصویر دیگه
+                alt="Pizza"
+                className=" mt-[9rem]  w-[90%] " // اندازه دلخواه
+                animate={{ rotate: 360 }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 20, // مدت زمان برای هر دور چرخش
+                  ease: "linear"
+                }}
+              />
+              <img className=" absolute top-[8rem]  left-[-2.8rem]  " src="https://modinatheme.com/foodking/wp-content/uploads/2024/02/frame-5.png" alt="" />
+              <img className=" absolute top-[7.2rem] right-[-2rem] z-[100]  " src="https://modinatheme.com/foodking/wp-content/uploads/2024/02/frame-2.png" alt="" />
+              <img className=" absolute top-[4rem] right-[40rem] " src="https://modinatheme.com/foodking/wp-content/uploads/2024/02/frame-3.png" alt="" />
+              <img className=" absolute top-[45rem]  left-[79.1rem] " src="https://modinatheme.com/foodking/wp-content/uploads/2024/02/frame-2-1.png" alt="" />
+              <img className=" absolute top-[52rem] left-[46em]  " src="https://modinatheme.com/foodking/wp-content/uploads/2024/02/frame-4.png" alt="" />
+              <img className=" absolute top-[47rem]  left-[-4.4rem] " src="https://t-theme.com/foodking/wp-content/uploads/2024/02/frame-6.png" alt="" />
+            </div>
+            <div className="w-[40%] z-[1000000] mt-[4.2rem] ">
+              <h1 className=" text-[#ffff]  font-semibold leading-[9.5rem] mt-[-11rem] text-[9rem] ml-auto ">پیتزا فوق العاده خوشمزه </h1>
+              <h4 className=" text-[#ffff] text-[1.4rem] font-semibold mt-[3rem] mb-[2rem] ml-auto  ">فقط با 25 هزارتومان سفارش خود را ستاره دار کنید</h4>
+              <button className=" ml-auto  outline-none transition-all ease delay-200 cursor-pointer hover:bg-[#ffff] bg-[#FFB936] flex items-center justify-center gap-[.5rem] text-center px-[1rem] w-[17rem] py-[1rem] pb-[1.3rem] font-bold ">
+                <span>
+                  <FaArrowLeft className=" mb-[-.5rem] " size={24} />
+                </span>{" "}
+                اکنون سفارش دهید
+              </button>
+            </div>
           </div>
-          <div className="w-[36%] ">
-            <h1 className=" text-[#ffff] font-semibold leading-[9.3rem]  text-[8.6rem] ml-auto ">پیتزای فوق العاده خوشمزه </h1>
-            <h4 className=" text-[#ffff] text-[1.4rem] font-semibold mt-[3rem] mb-[2rem] ml-auto  ">فقط با 25 هزارتومان سفارش خود را ستاره دار کنید</h4>
-          </div>
-        </div>
+        )}
       </section>
     </header>
   );
