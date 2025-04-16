de8ec1c4c5838e040a0bff9f431916507b993c77
amirreza <amirrezakarami008@gmail.com>
2025-04-16 22:48:40 +0330

    update

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,53 +6,42 @@ export default function Blog(){
             id: 1,
             title: 'سلام دنیا!',
             desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
+            image:'/pages/blog/b_image/b_01.jpg'
         },
         {
             id: 2,
+            title: 'آشپزی چارنیکلز در حال کاوش در شگفتی های غذا در رستوران فودکینگ',
+            desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
+            image:'/pages/blog/b_image/b_02.jpg'
+        },
+        {
+            id: 3,
             title: 'سلام دنیا!',
             desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
-        },
-        // {
-        //     id: 3,
-        //     title: 'سلام دنیا!',
-        //     desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
-        // },
-        // {
-        //     id: 4,
-        //     title: 'سلام دنیا!',
-        //     desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
-        // },
-        // {
-        //     id: 5,
-        //     title: 'سلام دنیا!',
-        //     desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
-        // },
-        // {
-        //     id: 6,
-        //     title: 'سلام دنیا!',
-        //     desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
-        // },
-        // {
-        //     id: 7,
-        //     title: 'سلام دنیا!',
-        //     desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
-        // },
-        // {
-        //     id: 8,
-        //     title: 'سلام دنیا!',
-        //     desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
-        // },
-        // {
-        //     id: 9,
-        //     title: 'سلام دنیا!',
-        //     desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
-        // },
-        // {
-        //     id: 10,
-        //     title: 'سلام دنیا!',
-        //     desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
-        // },
+            image:'/pages/blog/b_image/b_03.jpg'
 
+        },
+        {
+            id: 4,
+            title: 'سلام دنیا!',
+            desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
+            image:'/pages/blog/b_image/b_01.jpg'
+
+        },
+        {
+            id: 5,
+            title: 'سلام دنیا!',
+            desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
+            image:'/pages/blog/b_image/b_03.jpg'
+
+        },
+        {
+            id: 6,
+            title: 'سلام دنیا!',
+            desc: 'به وردپرس خوش آمدید. این اولین نوشتهٔ شماست. این را ویرایش یا حذف کنید، سپس نوشتن را شروع نمایید!',
+            image:'/pages/blog/b_image/b_02.jpg'
+
+        }
     ]
     return(
         ///bug
@@ -68,24 +57,55 @@ export default function Blog(){
                 </div>
             </div>
             <div className='grid grid-cols-1 lg:grid-cols-12 gap-4 mx-auto max-w-[82rem]  mt-[100px] '>
-            <div className='lg:col-span-8 px-3 space-y-4'>
+            <div className='lg:col-span-8  space-y-4'>
                 {dataBlog.map((item) => (
                     <BlogPage key={item.id} {...item} />
                 ))}
             </div>
-                <div className='lg:col-span-4 ps-5'>
-                    <div className='flex mb-[100px] justify-end'>
-                        <input type="text" className='border border-gray-300 p-3 px-5 w-full focus:outline-none focus:border-gray-300 ' style={{borderStartStartRadius : 'var(--wc-form-border-radius)'}}/>
+                <div className='lg:col-span-4 px-3'>
+                    <div className='flex mb-[100px] justify-end border py-[35px] px-3 border-gray-200'>
+                        <input type="text" className='border border-gray-300 p-4 px-5 w-full focus:outline-none focus:border-gray-300 ' style={{borderStartStartRadius : 'var(--wc-form-border-radius)'}}/>
                         <button className='px-5 py-3' style={{backgroundColor : 'var(--wc-orange)'}}>جستجو</button>
                     </div>
-                    <div className='bg-red-300'>
-                        <h3 className='text-2xl font-bold'>نوشته‌های تازه</h3>
+                    <div className='border py-[35px] px-3 border-gray-200 mb-[50px]'>
+                        <h3 className='text-2xl font-bold my-5'>نوشته‌های تازه</h3>
                         <ul>
-                            <li>سلام دنیا!</li>
-                            <li>شگفتی های غذا در رستوران فودکینگ</li>
-                            <li>آشپزی چارنیکلز در حال کاوش در شگفتی های غذا در رستوران</li>
-                            <li>نیش پررنگ: طعم های عجیب و غریب، ماجراجویی جهانی</li>
-                            <li>هوس های سریع: از بین بردن لذت های فست فود</li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">سلام دنیا!</a></li> 
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">شگفتی های غذا در رستوران فودکینگ</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">آشپزی چارنیکلز در حال کاوش در شگفتی های غذا در رستوران</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">نیش پررنگ: طعم های عجیب و غریب، ماجراجویی جهانی</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">هوس های سریع: از بین بردن لذت های فست فود</a></li>
+                        </ul>
+                    </div>
+                    <div className='border py-[35px] px-3 border-gray-200 my-[50px]'>
+                        <h3 className='text-2xl font-bold my-5'>نوشته‌های تازه</h3>
+                        <ul>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">آخرین دیدگاه‌ها</a></li> 
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">foodking <span className='text-[var(--wc-orange)]'>در</span> هوس های سریع: از بین بردن لذت های فست فود</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">یک نویسنده دیدگاه وردپرس <span className='text-[var(--wc-orange)]'>در</span>  سلام دنیا!</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">foodking <span className='text-[var(--wc-orange)]'>در</span>  هوپر برگر کینگ</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">foodking <span className='text-[var(--wc-orange)]'>در</span>  روتی با مرغ</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">foodking <span className='text-[var(--wc-orange)]'>در</span>  مرغ کبابی</a></li>
+                        </ul>
+                    </div>
+                    <div className='border py-[35px] px-3 border-gray-200 my-[50px]'>
+                        <h3 className='text-2xl font-bold my-5'>بایگانی‌ها</h3>
+                        <span className='text-xl'>جولای 2024</span>
+                    </div>
+                    <div className='border py-[35px] px-3 border-gray-200 my-[50px]'>
+                        <h3 className='text-2xl font-bold my-5'>دسته‌ها</h3>
+                        <ul>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">برنج کبابی</a></li> 
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">پاستا</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">پیتزا داغ</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">دسته‌بندی نشده</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">ساندویچ</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">سلامت و بهداشت</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">فرانسوی سرخ شده</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">فست فود</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">مرغ سوخاری</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">نان</a></li>
+                            <li className='my-4 text-xl hover:[color:var(--primaryColor)] transition-all'><a href="">هات داگ</a></li>
                         </ul>
                     </div>
                 </div>
--- components/blog_page/blogPage.tsx
+++ components/blog_page/blogPage.tsx
@@ -1,6 +1,7 @@
+import Image from "next/image";
 import { FaArrowLeft, FaCalendarAlt, FaComment, FaUser } from "react-icons/fa";
 
-export default function BlogPage({title , desc , data}) {
+export default function BlogPage({title , desc , data , image}) {
   return (
     <>
     <div className='flex'>
@@ -12,7 +13,14 @@ export default function BlogPage({title , desc , data}) {
         <h1 className='text-5xl font-bold'>{title}</h1>
         <h4 className='text-[18px] my-[40px] font-bold' style={{color : 'var(--textColor)'}}>{desc}</h4>
         <a href="#" className='flex items-center font-bold hover:[color:var(--primaryColor)] transition-all'><span className='me-1 text-xl'>مشاهده بیشتر </span><span><FaArrowLeft size='20px'/></span></a>
-        <div className='bg-[url(/pages/blog/b_image/b_01.jpg)]  bg-cover w-full h-[700px] my-[40px] rounded-3xl' ></div>
+        <div className='my-[40px] relative w-full lg:h-[700px] h-[600px]' >
+        <Image
+          className="rounded-2xl cover"
+          src={image}
+          alt="تصویر غذا"
+          fill
+        />
+        </div>
     </div>
     </>
                     
