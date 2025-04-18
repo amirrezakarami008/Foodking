7c3bce673abcb83d15efed0c5f2621cc3e25d839
atef <atefpoordehghan@gmail.com>
2025-04-18 17:13:04 +0330

    responsive 2

--- app/shop/testimonial/page.tsx
+++ app/shop/testimonial/page.tsx
@@ -58,9 +58,21 @@ export default function Testimonial() {
           style={{ strokeDashoffset }}
         />
       </motion.svg>
-      <div className="bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/07/breadcrumb-1.jpg)] w-full flex justify-center items-center h-95">
+      <div
+        className="
+     bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/07/breadcrumb-1.jpg)]
+      bg-cover bg-center  w-full flex justify-center items-center h-95"
+      >
         <div className=" space-y-5">
-          <h1 className="font-semibold  text-6xl text-white"> گواهینامه</h1>
+          <h1
+            className="w-full font-semibold  text-center text-3xl text-white
+            sm:text-3xl 
+            md:text-4xl 
+            lg:text-6xl lg:text-white "
+          >
+            {" "}
+            گواهینامه
+          </h1>
           <div className="flex space-x-1 items-center h-8 flex-row-reverse">
             <h1 className="font-semibold  text-xl text-green-500">
               {" "}
@@ -139,11 +151,32 @@ export default function Testimonial() {
       </div>
       {/* //! دیدار با کارشناس ها و سرآشپزها */}
       <div
-        className="w-full flex flex-wrap justify-end bg-[#e7ed70] mt-24 bg-no-repeat 
+        className="
+        justify-end
+
+md:justify-end
+
+        lg:justify-end
+        
+        xl:justify-end
+        2xl:justify-end
+      
+
+
+
+        w-full flex flex-wrap
+
+          bg-[#e7ed70] mt-24 
         bg-cover bg-center h-96
        bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/08/image.jpg)]"
       >
-        <section className="w-[188px]  flex justify-start items-center  h-full">
+        <section
+          className="
+         
+          w-16
+       
+         flex justify-start items-center  h-full"
+        >
           <motion.div
             // initial={{ y: 1 }}
             animate={{ translateY: [0, -200, 0] }}
@@ -154,13 +187,43 @@ export default function Testimonial() {
               repeat: Infinity,
               repeatType: "loop",
             }}
-            className="w-24 h-20 transition-all  duration-700
+            className="w-24   h-20 transition-all  duration-700
            bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/08/chilli.png)]"
           ></motion.div>
         </section>
-        <section className="w-1/5 flex justify-center items-center ">
+        <section
+          className="
+         
+          justify-start
+          w-auto
+        md:justify-center 
+        md:w-1/5 
+
+        lg:justify-center
+        lg:w-1/5 
+
+        xl:justify-center
+        xl:w-1/5 
+
+        2xl:justify-center 
+        2xl:w-1/5 
+
+
+        flex 
+        items-center "
+        >
           <button
-            className=" w-64 h-16 group relative
+            className="
+            w-48
+             md:w-64 
+
+             lg:w-64 
+
+             xl:w-64 
+
+             2xl:w-64 
+             
+            h-16 group relative
            font-semibold rounded-xl bg-slate-100"
           >
             <div
@@ -169,8 +232,25 @@ export default function Testimonial() {
               justify-center space-x-1
               font-semibold"
             >
-              <h1>اکنون سفارش دهید</h1>
-              <TbTruckDelivery className="text-green-700  transition-all duration-500 group-hover:text-white text-2xl" />
+              <h1
+                className="
+                font-semibold
+                text-xs
+               md:text-base
+
+             lg:text-base
+
+             xl:text-base
+
+             2xl:text-base
+              "
+              >
+                اکنون سفارش دهید
+              </h1>
+              <TbTruckDelivery
+                className="text-green-700
+               transition-all duration-500 group-hover:text-white text-2xl"
+              />
             </div>
             <span
               className="absolute inset-0 w-full h-full  rounded-xl bg-amber-400 
@@ -183,28 +263,107 @@ export default function Testimonial() {
           ref={motorCycleRef}
           transition={{ duration: 14 }}
           style={{ translateX: forMotorCycle as any }}
-          className=" transition-all duration-700 -translate-y-5 w-1/4 h-[350px]
-         bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/08/delivery-man-2.png)]"
+          className="
+          hidden
+          bg-cover bg-center
+
+bg-none
+md:hidden
+
+             lg:block
+
+             xl:block
+
+             2xl:block
+          md:bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/08/delivery-man-2.png)]
+md:-translate-x-36
+          lg:bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/08/delivery-man-2.png)]
+lg:-translate-x-36
+
+          xl:bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/08/delivery-man-2.png)]
+xl:-translate-x-36
+
+          2xl:bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/08/delivery-man-2.png)]
+2xl:-translate-x-36
+
+
+          w-1/4 h-[350px]
+          transition-all duration-700 -translate-y-5 
+         "
         ></motion.div>
-        <section className="w-1/3 h-full  flex justify-center items-center">
+        <section
+          className="
+    w-[220px]
+         justify-end
+         sm:w-[350px]
+     md:ml-6
+            md:w-[370px]
+
+             lg:ml-6
+             lg:w-[450px]
+
+
+             xl:ml-24
+             xl:w-[470px]
+
+             2xl:ml-24
+         
+         h-full  flex  items-center"
+        >
           <div className="w-full h-1/2">
-            <h1 className="text-amber-300 text-xl text-right w-full font-semibold ">
+            <h1
+              className="text-amber-300
+            text-xl text-right w-full font-semibold "
+            >
               ترد، هر لقمه طعم
             </h1>
-            <h1 className="text-white text-right text-6xl  w-full font-semibold ">
+            <h1
+              className="
+              text-3xl
+              md:text-4xl 
+
+             lg:text-5xl 
+
+             xl:text-6xl 
+
+             2xl:text-6xl 
+             
+            text-white text-right
+              w-full font-semibold "
+            >
               دقیقه سریع 45
             </h1>
             <div className="flex  justify-end">
-              <h1 className="text-white text-right text-6xl   font-semibold ">
+              <h1
+                className="text-white text-right 
+                 text-2xl
+              md:text-4xl 
+
+             lg:text-6xl 
+
+             xl:text-6xl 
+
+             2xl:text-6xl 
+              font-semibold "
+              >
                 قول ما
               </h1>
-              <h1 className="text-amber-300 text-right text-6xl   font-semibold ">
+              <h1
+                className="
+   text-2xl
+              md:text-4xl 
+
+             lg:text-6xl 
+
+             xl:text-6xl 
+
+             2xl:text-6xl 
+              text-amber-300 text-right   font-semibold "
+              >
                 تحویل
               </h1>
             </div>
           </div>
-        </section>
-        <section className="w-56 flex justify-end items-center  h-full">
           <motion.div
             // initial={{ y: 1 }}
             animate={{ translateY: [100, -200, 200] }}
@@ -215,7 +374,20 @@ export default function Testimonial() {
               repeat: Infinity,
               repeatType: "loop",
             }}
-            className="w-24 h-1/2 transition-all duration-700
+            className="
+           
+            
+w-24
+
+            md:w-24
+
+             lg:w-24
+
+             xl:w-24
+
+             2xl:w-24
+            
+            h-1/2 transition-all duration-700
            bg-[url(https://t-theme.com/foodking/wp-content/uploads/2024/08/image-11.png)]"
           ></motion.div>
         </section>
